7504b61647483f6f26ae22274571a0067618c325
Jumpei Mori <nikutto.kom@gmail.com>
2020-10-12 14:40:10 +0900

    Add hello world test

---
+++ src/test/kotlin/com/example/demo/IntegrationTests.kt
@@ -0,0 +1,20 @@
+package com.example.demo
+
+import org.assertj.core.api.Assertions.*
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.web.client.TestRestTemplate
+import org.springframework.boot.test.web.client.getForEntity
+import org.springframework.http.HttpStatus
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class IntegrationTest(@Autowired val restTemplate: TestRestTemplate) {
+
+    @Test
+    fun `Assert hello world works correctly`() {
+        val entitty = restTemplate.getForEntity<String>("/")
+        assertThat(entitty.statusCode).isEqualTo(HttpStatus.OK)
+        assertThat(entitty.body).contains("<h1>Hello world!</h1>")
+    }
+}
