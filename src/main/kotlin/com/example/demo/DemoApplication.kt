398b44916ce4917b855fe54a0389267cae171c6b
Jumpei Mori <nikutto.kom@gmail.com>
2020-10-17 15:46:09 +0900

    lint mainSource

--- src/main/kotlin/com/example/demo/DemoApplication.kt
+++ src/main/kotlin/com/example/demo/DemoApplication.kt
@@ -1,13 +1,12 @@
 package com.example.demo
-
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.runApplication
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
+import org.springframework.boot.runApplication
 
 @ConfigurationPropertiesScan
 @SpringBootApplication
 class DemoApplication
 
 fun main(args: Array<String>) {
-	runApplication<DemoApplication>(*args)
+    runApplication<DemoApplication>(*args)
 }
